7b2d69af47014669009cd54d3887b1c7527fc716
Skyedown <lehocky.peter@yahoo.com>
2026-06-05 21:29:27 +0200

    changed text of card payment

--- src/components/PizzaCart/PaymentMethodSelector/PaymentMethodSelector.tsx
+++ src/components/PizzaCart/PaymentMethodSelector/PaymentMethodSelector.tsx
@@ -80,7 +80,7 @@ const PaymentMethodSelector: React.FC<PaymentMethodSelectorProps> = ({
             <rect x="1" y="4" width="22" height="16" rx="2" ry="2" />
             <line x1="1" y1="10" x2="23" y2="10" />
           </svg>
-          <span>Kartou</span>
+          <span>Kartou pri prevzatí</span>
         </div>
         {!isCardPaymentEnabled && (
           <span className="payment-method__badge">Momentálne nedostupné</span>
